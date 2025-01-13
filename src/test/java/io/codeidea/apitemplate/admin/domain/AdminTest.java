b4e3a75089bf46865795098a646188d1b531ae7f
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-13 13:36:12 +0900

    :white_check_mark: Admin 로그인, 생성 테스트 추가

--- src/test/java/io/codeidea/apitemplate/admin/domain/AdminTest.java
+++ src/test/java/io/codeidea/apitemplate/admin/domain/AdminTest.java
@@ -19,6 +19,26 @@ class AdminTest {
     }
 
     @Test
+    void 관리자는_생성될_수_있다() {
+        // given
+        LocalDateTime createdAt = LocalDateTime.now();
+
+        // when
+        Admin admin =
+                Admin.create(1L, "foo", "bar", passwordEncoder.encode("baz"), AdminRole.NORMAL, createdAt, createdAt, null);
+
+        // then
+        assertThat(admin.getId()).isEqualTo(1L);
+        assertThat(admin.getName()).isEqualTo("foo");
+        assertThat(admin.getLoginId()).isEqualTo("bar");
+        assertThat(passwordEncoder.matches("baz", admin.getPassword())).isTrue();
+        assertThat(admin.getRole()).isEqualTo(AdminRole.NORMAL);
+        assertThat(admin.getCreatedAt()).isEqualTo(createdAt);
+        assertThat(admin.getUpdatedAt()).isEqualTo(createdAt);
+        assertThat(admin.getLastLoginAt()).isNull();
+    }
+
+    @Test
     void 관리자는_회원가입을_할_수_있다() {
         // given
         AdminSignUp adminSignUp = new AdminSignUp("foo", "bar", "baz", AdminRole.NORMAL);
@@ -64,5 +84,21 @@ class AdminTest {
         assertThat(updatedAdmin.getCreatedAt()).isEqualTo(createdAt);
         assertThat(updatedAdmin.getUpdatedAt()).isEqualTo(updatedAt);
         assertThat(updatedAdmin.getLastLoginAt()).isNull();
+    }
+
+    @Test
+    void 관리자는_로그인을_할_수_있다() {
+        // given
+        LocalDateTime createdAt = LocalDateTime.now();
+        Admin admin =
+                Admin.create(1L, "foo", "bar", passwordEncoder.encode("baz"), AdminRole.NORMAL, createdAt, createdAt, null);
+        LocalDateTime lastLoginAt = LocalDateTime.now();
+
+        // when
+        boolean result = admin.signIn("baz", lastLoginAt, passwordEncoder);
+
+        // then
+        assertThat(result).isTrue();
+        assertThat(admin.getLastLoginAt()).isEqualTo(lastLoginAt);
     }
 }
