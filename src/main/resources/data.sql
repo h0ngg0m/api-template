162ab53e81184718868590210bb1568e9af35180
h0ngg0m <jaehongbyeon98@gmail.com>
2025-01-29 17:28:49 +0900

    :hammer: 공지사항 기본 데이터 insert sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,13 @@ VALUES (1, 'admin', 'admin', '$2a$10$7eVxlG3WEZqGksKO/u3NC.9VZyVbacL9GO73LVi1k06
      , (6, 'admin6', 'admin6', '$2a$10$7eVxlG3WEZqGksKO/u3NC.9VZyVbacL9GO73LVi1k06uqiX8qx4sy', 'SUPER', now(), now())
      , (7, 'admin7', 'admin7', '$2a$10$7eVxlG3WEZqGksKO/u3NC.9VZyVbacL9GO73LVi1k06uqiX8qx4sy', 'SUPER', now(), now())
      , (8, 'admin8', 'admin8', '$2a$10$7eVxlG3WEZqGksKO/u3NC.9VZyVbacL9GO73LVi1k06uqiX8qx4sy', 'SUPER', now(), now());
+
+INSERT INTO notice (ID, TITLE, CONTENT, CREATED_AT, UPDATED_AT)
+VALUES (1, '공지사항1', '공지사항1 내용', now(), now())
+     , (2, '공지사항2', '공지사항2 내용', now(), now())
+     , (3, '공지사항3', '공지사항3 내용', now(), now())
+     , (4, '공지사항4', '공지사항4 내용', now(), now())
+     , (5, '공지사항5', '공지사항5 내용', now(), now())
+     , (6, '공지사항6', '공지사항6 내용', now(), now())
+     , (7, '공지사항7', '공지사항7 내용', now(), now())
+     , (8, '공지사항8', '공지사항8 내용', now(), now());
